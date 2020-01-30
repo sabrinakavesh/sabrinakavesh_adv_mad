14ea0f8fc76fdbc0045abec6a10f79762edc9b4f
sabrinaKav <sabrina.kavesh@colorado.edu>
2020-01-30 09:01:48 -0700

    new

--- inclass/music/music/ArtistAlbumsController.swift
+++ inclass/music/music/ArtistAlbumsController.swift
@@ -8,23 +8,63 @@
 
 import Foundation
 
+enum DataError: Error {
+    case BadFilePath
+    case CouldNotDecodeData
+    case NoData
+}
+
 class ArtistAlbumsController { //same name as file
     //properties
     var artistAlbumsData: [ArtistAlbums]? //made optional so when we load data later thres a chance it could fail so wouldn't have any data so don't want to assume we will have data when its possible we wont
     let filename = "artist-albums"
     
     //read from plist and decode to array of ArtistAlbums struct
-    func loadData() {
+    func loadData() throws { //throws: eitehr this function will execute correctly or we will throw an error
+        print("Loading Data....")
         
+        if let pathURL = Bundle.main.url(forResource: filename, withExtension: "plist") {
+            //found the file
+            let decoder = PropertyListDecoder()
+            
+            do {
+                let data = try Data(contentsOf: pathURL)
+                artistAlbumsData = try decoder.decode([ArtistAlbums].self, from: data)
+                print("Data loaded")
+            } catch {
+                throw DataError.CouldNotDecodeData
+            }
+            
+        } else {
+            throw DataError.BadFilePath
+        }
     }
     
     //get all the artists and return array of strings
-    func getAllArtists() -> [String] {
+    func getAllArtists() throws -> [String] {
+        var artists = [String]()
         
+        if let data = artistAlbumsData {
+            //we have data
+            for artistStruct in data {
+                artists.append(artistStruct.name)
+            }
+            return artists
+        } else {
+            throw DataError.NoData
+        }
     }
     
     //get all the albums for any of the artists
-    func getAlbums(idx: Int) {
+    func getAlbums(idx: Int) throws -> [String] {
+        //make sure we have dat
+        if let data = artistAlbumsData {
+            //good data
+            return data[idx].albums
+        } else {
+            //no dat
+            throw DataError.NoData
+        }
         
     }
     
---
+++ inclass/music/music/FifthViewController.swift
@@ -0,0 +1,78 @@
+//
+//  FifthViewController.swift
+//  music
+//
+//  Created by Sabrina on 1/28/20.
+//  Copyright © 2020 Sabrina. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class FifthViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+
+    //variables
+    let audioSession = AVAudioSession.sharedInstance()
+    var audioPlayer: AVAudioPlayer?
+    var audioRecorder: AVAudioRecorder?
+    
+    //constant file name so always overwriting insetead of creating new
+    let filename = "audio.m4a" //can also use aif possibly
+    
+    
+    //connections
+    @IBOutlet weak var recordAudio: UIButton!
+    @IBOutlet weak var stopAudio: UIButton!
+    @IBOutlet weak var playAudio: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let dirPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) //userdomainmask: portion of system where user gets to modify files
+        //above, returns an array
+        let docDir = dirPath[0] //only want first one
+        let audioFilePath = docDir.appendingPathComponent(filename)
+        print(audioFilePath)
+        
+        //configre audio session
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.playAndRecord, mode: .default, options: .init(rawValue: 1)) //options set volume baseline
+        } catch {
+            print(error)
+        }
+        
+        //dictionary of settings, these are pretty standard settings
+        let settings = [
+            //dif settings listed on avaudiorecorder documentation, some required
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC), //audio codec
+            AVSampleRateKey: 1200, //sample rate hZ
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue //bit rate
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilePath, settings: settings)
+            audioRecorder?.prepareToRecord() //assuming we got audio recordder we need to prepar it to record
+            print("Audio recorder ready!")
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    @IBAction func recordAudio(_ sender: Any) {
+    
+    }
+    
+    @IBAction func playAudio(_ sender: Any) {
+    
+    }
+    
+    @IBAction func stopAudio(_ sender: Any) {
+    
+    }
+    
+
+}
